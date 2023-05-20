f6399fe300ba449b6849a38b06bf2ddfd923ef7a
Afra <propazaman12@gmail.com>
2023-05-20 13:28:40 +0600

    login handle

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/fairy-tales-logo.png'
+import { AuthContext } from '../../Provider/AuthProvider';
 
 const Navbar = () => {
+
+    const { user, handleSignout } = useContext(AuthContext);
+    const handleLogout = () => {
+        handleSignout();
+    };
 
     const navItems = <>
         <li><Link to="/">Home</Link> </li>
         <li> <Link to="/alltoys">All Toys</Link> </li>
-        {/* {user?.email ? <>
-            <li><Link to="/bookings">My Bookings</Link></li>
-            <li><button onClick={handleLogOut}>Log out</button></li>
-        </>  :*/}
         <li> <Link to="/blog">Blog</Link> </li>
+
+        <>
+            {user ? (
+                <>
+                    <li> <Link to="/addtoy">Add A Toy</Link> </li>
+                    <li> <Link to="/mytoys">My Toys</Link> </li>
+                    {user.photoURL && (
+                        <img title={user.displayName}
+                            className="h-10 rounded-full border-2 border-primary"
+                            src={user?.photoURL}
+                            alt=""
+                        />
+                    )}
+                  
+                    <button onClick={handleLogout} className="ml-5 btn btn-primary text-white">
+                        Logout
+                    </button>
+                 
+                </>
+            ) : (
+                <Link to="/login">
+                    <button className="btn btn-primary">Login</button>
+                </Link>
+            )}
+        </>
+
+
 
     </>
     return (
@@ -21,7 +50,7 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         {navItems}
                     </ul>
                 </div>
@@ -29,7 +58,7 @@ const Navbar = () => {
                     <img src={logo} alt="" className="w-full h-full" />
                 </Link>
             </div>
-            <div className="navbar-end hidden font-bold text-primary text-2xl lg:flex">
+            <div className="navbar-center hidden font-bold text-primary text-2xl lg:flex">
                 <ul className="menu menu-horizontal px-1">
                     {navItems}
                 </ul>
