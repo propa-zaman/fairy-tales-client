daf5159a7de4f2a53ff0bf9e4cc6c75d178ad4d5
Afra <propazaman12@gmail.com>
2023-05-21 06:51:04 +0600

    shop by category complete

--- src/pages/Shop by Category/ShopByCategory.jsx
+++ src/pages/Shop by Category/ShopByCategory.jsx
@@ -19,12 +19,12 @@ const ShopByCategory = () => {
 
             <div className="flex justify-center items-center h-screen">
                 <div className="w-full max-w-lg">
-                    <Tabs className="w-full" selectedTabClassName="border-b-2 border-blue-500">
+                    <Tabs className="w-full" selectedTabClassName="border-b-2 border-neutral">
                         <TabList className="flex">
                             {categories.map((category, index) => (
                                 <Tab
                                     key={index}
-                                    className={`text-gray-500 font-semibold py-2 px-4 transition-colors duration-200 focus:outline-none hover:text-blue-500 ${index === 0 ? 'text-blue-500' : ''
+                                    className={`text-gray-500 font-semibold py-2 px-4 transition-colors duration-200 focus:outline-none hover:text-neutral ${index === 0 ? 'text-primary' : ''
                                         }`}
                                 >
                                     {category.categoryName}
@@ -34,9 +34,9 @@ const ShopByCategory = () => {
 
                         {categories.map((category, index) => (
                             <TabPanel key={index} className="p-4 h-full w-full">
-                                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4">
                                     {category.dolls.map((doll) => (
-                                        <div key={doll.id} className="bg-white rounded-lg shadow-md p-4">
+                                        <div key={doll.id} className="bg-white rounded-lg shadow-md p-4 w-full">
                                             <div className="mb-4">
                                                 <img
                                                     src={doll.picture}
@@ -49,8 +49,8 @@ const ShopByCategory = () => {
                                                 <p>Price: {doll.price}</p>
                                                 <p>Rating: {doll.rating}</p>
                                             </div>
-                                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                                Add to Cart
+                                            <button className="bg-primary hover:bg-neutral text-white font-bold py-2 px-4 rounded">
+                                                View Details
                                             </button>
                                         </div>
                                     ))}
